c8261bfa162d3ce0c925bc3ea4b6b89db8aa281d
JunMasaki59 <jy13.27.813@gmail.com>
2024-07-16 17:26:41 +0900

    AMI修正

--- lessonDCS/aws_ec2.tf
+++ lessonDCS/aws_ec2.tf
@@ -7,7 +7,7 @@
 #   設定項目 = "値"
 # }
 resource "aws_instance" "sample_web_server" {
-  ami                    = "ami-013a28d7c2ea10269"       # Amazon Linux 2023 AMI
+  ami                    = "ami-0eda63ec8af4f056e"           # Amazon Linux 2 AMI
   instance_type          = "t2.micro"                        # インスタンスタイプ
   subnet_id              = aws_subnet.sample_subnet.id       # サブネットのID
   vpc_security_group_ids = [aws_security_group.sample_sg.id] # セキュリティグループのID
